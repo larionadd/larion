4d378ca9c810da6b2aa93e77d87f16db20565b80
larionadd <larionadd@gmail.com>
2017-09-04 21:17:19 +0300

    tree1

---
+++ HelloJava/src/tree1.java
@@ -0,0 +1,14 @@
+public class tree1{
+    public static void main (String[] args){
+        System.out.println("        /\\\n");
+        System.out.println("       /  \\\n");
+        System.out.println("      / || \\\n");
+        System.out.println("     /  ||  \\\n");
+        System.out.println("    /   ||   \\\n");
+        System.out.println("   /    ||    \\\n");
+        System.out.println("  /     ||     \\\n");
+        System.out.println("        ||       ");
+        System.out.println("                 ");
+        System.out.println("_________________");
+        }
+}
